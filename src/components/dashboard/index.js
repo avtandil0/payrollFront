2c033890542648240013cb3db321db8e8e0b38e0
azenaishvili <azenaishvili@AZENAISHVILI1>
2021-12-24 16:34:54 +0400

    update

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,30 +1,240 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
 
-
+import { Row, Col, Button } from "antd";
+import { Statistic, Card, Progress, Alert, Space } from "antd";
+import {
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+  LikeOutlined,
+  RiseOutlined,
+} from "@ant-design/icons";
 
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    useParams,
-    useRouteMatch,
-    useLocation
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  useParams,
+  useRouteMatch,
+  useLocation,
 } from "react-router-dom";
 
-
-
-
 function Dashboard() {
+  const { Countdown } = Statistic;
+  const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
 
+  function onFinish() {
+    console.log("finished!");
+  }
 
-    return (
-        <div>
-            dashboard
-        </div>
-    );
+  function onChange(val) {
+    if (4.95 * 1000 < val && val < 5 * 1000) {
+      console.log("changed!");
+    }
+  }
+
+  return (
+    <div>
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Row gutter={16}>
+            <Col span={12}>
+              <div style={{ display: "flex" }}>
+              <Space size="large">
+                <div>
+                  <Statistic title="Active Users" value={1460} />
+                </div>
+                <div >
+                  <Statistic title="Passive Users" value={379} />
+                </div>
+                <div >
+                  <Statistic title="Analitics" value={1000} />
+                </div>
+                <div >
+                  <Statistic title="Operators" value={460} />
+                </div>
+                </Space>
+              </div>
+            </Col>
+            <Col span={12}>
+              <Statistic
+                title="Account Balance (CNY)"
+                value={112893}
+                precision={2}
+              />
+              <Button style={{ marginTop: 16 }} type="primary">
+                Recharge
+              </Button>
+            </Col>
+            <Col span={12}>
+              <Statistic title="Active Users" value={112893} loading />
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="Active"
+                  value={11.28}
+                  precision={2}
+                  valueStyle={{ color: "#3f8600" }}
+                  prefix={<ArrowUpOutlined />}
+                  suffix="%"
+                />
+                <br />
+                <Progress percent={30} />
+                <Progress percent={50} status="active" />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="Idle"
+                  value={9.3}
+                  precision={2}
+                  valueStyle={{ color: "#cf1322" }}
+                  prefix={<ArrowDownOutlined />}
+                  suffix="%"
+                />
+                <br />
+                <Progress percent={70} status="exception" />
+                <Progress percent={100} />
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <br />
+      <br />
+
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Row gutter={16}>
+            <Col span={12}>
+              <Statistic
+                title="Feedback"
+                value={1128}
+                prefix={<LikeOutlined />}
+              />
+            </Col>
+            <Col span={12}>
+              <Statistic title="Unmerged" value={93} suffix="/ 100" />
+            </Col>
+          </Row>
+          <br />
+          <br />
+          <Row gutter={16}>
+            <Col span={12}>
+              <Statistic
+                title="Feedback"
+                value={400.456}
+                prefix={<RiseOutlined />}
+              />
+            </Col>
+            <Col span={12}>
+              <Statistic title="Test" value={235} suffix="/ 405" />
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Row gutter={16}>
+            <Col span={12}>
+              <Countdown
+                title="Countdown"
+                value={deadline}
+                onFinish={onFinish}
+              />
+            </Col>
+            <Col span={12}>
+              <Countdown
+                title="Million Seconds"
+                value={deadline}
+                format="HH:mm:ss:SSS"
+              />
+            </Col>
+            <Col span={24} style={{ marginTop: 32 }}>
+              <Countdown
+                title="Day Level"
+                value={deadline}
+                format="HH:mm:ss:SSS"
+              />
+            </Col>
+            <Col span={12}>
+              <Countdown
+                title="Countdown"
+                value={Date.now() + 10 * 1000}
+                onChange={onChange}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <br />
+      <br />
+      <Row>
+        <Col span={10}>
+          <Progress
+            strokeColor={{
+              "0%": "#108ee9",
+              "100%": "#87d068",
+            }}
+            percent={91.9}
+          />
+          <Progress
+            strokeColor={{
+              from: "#108ee9",
+              to: "#87d068",
+            }}
+            percent={99.9}
+            status="active"
+          />
+          <br />
+          <br />
+          <Progress
+            type="circle"
+            strokeColor={{
+              "0%": "#108ee9",
+              "100%": "#87d068",
+            }}
+            percent={90}
+          />
+          <Progress
+            type="circle"
+            strokeColor={{
+              "0%": "#108ee9",
+              "100%": "#87d068",
+            }}
+            percent={45}
+          />{" "}
+        </Col>
+        <Col span={2}></Col>
+        <Col span={11}>
+          <Alert
+            message="Success Tips"
+            description="Detailed description and advice about successful copywriting."
+            type="success"
+            showIcon
+          />
+          <br />
+          <Alert
+            message="Warning"
+            description="This is a warning notice about copywriting."
+            type="warning"
+            showIcon
+            closable
+          />
+          <br />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 export default Dashboard;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Layout as LayoutAnt, Select, Avatar, Menu, Dropdown, Space, Divider } from 'antd';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import {
+  Layout as LayoutAnt,
+  Select,
+  Avatar,
+  Menu,
+  Dropdown,
+  Space,
+  Divider,
+} from "antd";
 
 import {
   MenuUnfoldOutlined,
@@ -19,24 +27,23 @@ import {
   GlobalOutlined,
   UserOutlined,
   UnlockOutlined,
-  LogoutOutlined
-} from '@ant-design/icons';
+  LogoutOutlined,
+  DashboardOutlined,
+} from "@ant-design/icons";
 
-import Component from '../component/index';
-import Department from '../department/index';
-import Coefficient from '../coefficient/index';
+import Component from "../component/index";
+import Department from "../department/index";
+import Coefficient from "../coefficient/index";
 // import AccountPlan from '../accountPlan/index';
-import Employee from '../employee/index';
-import Project from '../project/index';
-import CostCenter from '../costCenter/index';
-import AccountsReportChart from '../accountsReportChart/index';
-import EmployeeDetails from '../employee/employeeDetails';
-import EmployeeByDepartment from '../department/employeeByDepartment';
-import Calculate from '../calculate';
-import Dashboard from '../dashboard';
+import Employee from "../employee/index";
+import Project from "../project/index";
+import CostCenter from "../costCenter/index";
+import AccountsReportChart from "../accountsReportChart/index";
+import EmployeeDetails from "../employee/employeeDetails";
+import EmployeeByDepartment from "../department/employeeByDepartment";
+import Calculate from "../calculate";
+import Dashboard from "../dashboard";
 import { useTranslation } from "react-i18next";
-
-
 
 import {
   BrowserRouter as Router,
@@ -45,23 +52,20 @@ import {
   Redirect,
   useParams,
   useRouteMatch,
-  useLocation
+  useLocation,
 } from "react-router-dom";
 
 import { useHistory } from "react-router-dom";
-import { HOME_PAGE } from '../../constant';
-import GE from '../../assets/logos/ge.png'; // with import
-import EN from '../../assets/logos/en.png'; // with import
-import RU from '../../assets/logos/ru.png'; // with import
-
+import { HOME_PAGE } from "../../constant";
+import GE from "../../assets/logos/ge.png"; // with import
+import EN from "../../assets/logos/en.png"; // with import
+import RU from "../../assets/logos/ru.png"; // with import
 
 const { Header, Sider, Content } = LayoutAnt;
-
 
 const { SubMenu } = Menu;
 
 const { Option } = Select;
-
 
 function Home() {
   let history = useHistory();
@@ -74,71 +78,69 @@ function Home() {
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    if (location.pathname == '/payroll/component') {
-      setActiveUrl(['1']);
+    if (location.pathname == "/payroll/component") {
+      setActiveUrl(["1"]);
     }
 
-    if (location.pathname == '/payroll/coefficient') {
-      setActiveUrl(['2']);
+    if (location.pathname == "/payroll/coefficient") {
+      setActiveUrl(["2"]);
     }
     // if( location.pathname == '/Reports/accountPlan'){
     //   setActiveUrl(['4']);
     // }
-    if (location.pathname == '/payroll/employee') {
-      setActiveUrl(['3']);
+    if (location.pathname == "/payroll/employee") {
+      setActiveUrl(["3"]);
     }
-    if (location.pathname == '/payroll/project') {
-      setActiveUrl(['4']);
+    if (location.pathname == "/payroll/project") {
+      setActiveUrl(["4"]);
     }
-    if (location.pathname == '/payroll/costCenter') {
-      setActiveUrl(['5']);
+    if (location.pathname == "/payroll/costCenter") {
+      setActiveUrl(["5"]);
     }
-    if (location.pathname == '/payroll/accountsReportChart') {
-      setActiveUrl(['6']);
+    if (location.pathname == "/payroll/accountsReportChart") {
+      setActiveUrl(["6"]);
     }
-    if (location.pathname == '/payroll/department') {
-      setActiveUrl(['7']);
+    if (location.pathname == "/payroll/department") {
+      setActiveUrl(["7"]);
     }
-    if (location.pathname == '/payroll/calculate') {
-      setActiveUrl(['8']);
+    if (location.pathname == "/payroll/calculate") {
+      setActiveUrl(["8"]);
     }
-    if (location.pathname == '/payroll/dashboard') {
-      setActiveUrl(['9']);
+    if (location.pathname == "/payroll/dashboard") {
+      setActiveUrl(["9"]);
     }
-
   }, []);
 
   const toggle = () => {
-    setCollapsed(!collapsed)
+    setCollapsed(!collapsed);
   };
 
-  
-  const ClickGoPage = (e, name) =>{
-    console.log("ClickGoPage",  e.key, name)
+  const ClickGoPage = (e, name) => {
+    console.log("ClickGoPage", e.key, name);
     // setActiveUrl({name: e.key})
-    setActiveUrl([e.key])
+    setActiveUrl([e.key]);
     history.push(`${HOME_PAGE}/${name}`);
-  }
+  };
 
-  const handleChange = ({key}) => {
-    i18n.changeLanguage(key)
-  }
+  const handleChange = ({ key }) => {
+    i18n.changeLanguage(key);
+  };
 
   const languageMenu = (
     <Menu onClick={handleChange}>
       <Menu.Item key="ge">
         <Space>
-          <img width={25} height={20} alt='GE' src={GE} /> <span>Georgian</span>
+          <img width={25} height={20} alt="GE" src={GE} /> <span>Georgian</span>
         </Space>
       </Menu.Item>
-      <Menu.Item key="en" >
+      <Menu.Item key="en">
         <Space>
-          <img width={25} height={20} alt='EN' src={EN} /> <span>English</span>
+          <img width={25} height={20} alt="EN" src={EN} /> <span>English</span>
         </Space>
       </Menu.Item>
       <Menu.Item key="ru">
         <Space>
-          <img width={25} height={20} alt='RU' src={RU} /> <span>Russian</span>
+          <img width={25} height={20} alt="RU" src={RU} /> <span>Russian</span>
         </Space>
       </Menu.Item>
     </Menu>
@@ -151,15 +153,17 @@ function Home() {
           <UserOutlined /> Settings
         </Space>
       </Menu.Item>
-      <Menu.Item  >
+      <Menu.Item>
         <Space>
-          <UnlockOutlined />Change Password
+          <UnlockOutlined />
+          Change Password
         </Space>
       </Menu.Item>
       <Menu.Divider />
       <Menu.Item>
         <Space>
-          <LogoutOutlined />Log Out
+          <LogoutOutlined />
+          Log Out
         </Space>
       </Menu.Item>
     </Menu>
@@ -174,47 +178,86 @@ function Home() {
           position: "sticky",
           top: 0,
         }}
-        theme="light" trigger={null} collapsible collapsed={collapsed} width={260}
+        theme="light"
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        width={260}
       >
-        <div className="logo" >
+        <div className="logo">
           {/* <img alt="aaa" style={{width:'100%', height: 'auto'}} src={"http://static1.squarespace.com/static/595bc3af29687ff67d144e54/t/5fff75649475673f7e479897/1610577255521/Payroll+logo.png?format=1500w"} /> */}
           {/* <img alt="aaa" style={{width:'100%', height: 'auto'}} src={Logo} /> */}
-
         </div>
-        <Menu selectedKeys={activeUrl} theme="light" mode="inline" >
-        <Menu.Item key="9" icon={<DatabaseOutlined />} title="dashboard" onClick={(e) =>ClickGoPage(e, "dashboard")}>
-              {/* {t(`dashboard`)} */}dashboard
-            </Menu.Item>
-          <Menu.Item key="8" icon={<CalculatorOutlined />} onClick={(e) =>ClickGoPage(e, "calculate")}>
+        <Menu selectedKeys={activeUrl} theme="light" mode="inline">
+          <Menu.Item
+            key="9"
+            icon={<DashboardOutlined />}
+            onClick={(e) => ClickGoPage(e, "dashboard")}
+          >
+            {t(`dashboard`)}
+          </Menu.Item>
+          <Menu.Item
+            key="8"
+            icon={<CalculatorOutlined />}
+            onClick={(e) => ClickGoPage(e, "calculate")}
+          >
             {t(`calculation`)}
           </Menu.Item>
-          <Menu.Item key="1" icon={<FundOutlined />} name="component" onClick={(e) =>ClickGoPage(e, "component")}>
+          <Menu.Item
+            key="1"
+            icon={<FundOutlined />}
+            name="component"
+            onClick={(e) => ClickGoPage(e, "component")}
+          >
             {t(`component`)}
           </Menu.Item>
-          <Menu.Item key="2" icon={<FunctionOutlined />} onClick={(e) =>ClickGoPage(e, "coefficient")}>
+          <Menu.Item
+            key="2"
+            icon={<FunctionOutlined />}
+            onClick={(e) => ClickGoPage(e, "coefficient")}
+          >
             {t(`coefficient`)}
           </Menu.Item>
           {/* <Menu.Item key="4" icon={<AccountBookOutlined />} onClick={clickAccountPlan}>
           accountPlan
           </Menu.Item> */}
-          <Menu.Item key="3" icon={<UsergroupAddOutlined />} onClick={(e) =>ClickGoPage(e, "employee")}>
+          <Menu.Item
+            key="3"
+            icon={<UsergroupAddOutlined />}
+            onClick={(e) => ClickGoPage(e, "employee")}
+          >
             {t(`employee`)}
           </Menu.Item>
 
           <SubMenu key="sub1" title={t(`setting`)} icon={<SettingOutlined />}>
-            <Menu.Item key="4" icon={<ProjectOutlined />} onClick={(e) =>ClickGoPage(e, "project")}>
+            <Menu.Item
+              key="4"
+              icon={<ProjectOutlined />}
+              onClick={(e) => ClickGoPage(e, "project")}
+            >
               {t(`project`)}
             </Menu.Item>
-            <Menu.Item key="5" icon={<StrikethroughOutlined />} onClick={(e) =>ClickGoPage(e, "costCenter")}>
+            <Menu.Item
+              key="5"
+              icon={<StrikethroughOutlined />}
+              onClick={(e) => ClickGoPage(e, "costCenter")}
+            >
               {t(`costCenter`)}
             </Menu.Item>
-            <Menu.Item key="6" icon={<DiffOutlined />} onClick={(e) =>ClickGoPage(e, "accountsReportChart")}>
+            <Menu.Item
+              key="6"
+              icon={<DiffOutlined />}
+              onClick={(e) => ClickGoPage(e, "accountsReportChart")}
+            >
               accountsReportChart
             </Menu.Item>
-            <Menu.Item key="7" icon={<DatabaseOutlined />} onClick={(e) =>ClickGoPage(e, "department")}>
+            <Menu.Item
+              key="7"
+              icon={<DatabaseOutlined />}
+              onClick={(e) => ClickGoPage(e, "department")}
+            >
               {t(`department`)}
             </Menu.Item>
-
           </SubMenu>
         </Menu>
       </Sider>
@@ -223,66 +266,69 @@ function Home() {
         <Header
           style={{
             // position: 'fixed',
-            zIndex: 1, width: '100%',
-            backgroundColor: '#f5f7f7'
+            zIndex: 1,
+            width: "100%",
+            backgroundColor: "#f5f7f7",
           }}
-          className="site-layout-background" >
-
-
-
+          className="site-layout-background"
+        >
           {/* <div style={{ display: "flex"}}> */}
 
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: toggle,
-          })}
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: toggle,
+            }
+          )}
 
-
-
-
-          <div style={{ float: 'right', right: 0 }}>
+          <div style={{ float: "right", right: 0 }}>
             {/* <Select defaultValue="en" style={{ width: 120 }} onChange={handleChange}>
               <Option value="en">En</Option>
               <Option value="ge">Ge</Option>
 
             </Select> */}
 
-            <Space size={'large'}>
-              <Dropdown overlay={userMenu}>
-                <div style={{ cursor: 'pointer' }}>
-                  <Space>
-                    <Avatar size={32} icon={<UserOutlined />}/>
-                    <span>Avtandil</span>
-                  </Space>
-                </div>
-              </Dropdown>
-
-              <Dropdown overlay={languageMenu}>
-                <div className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                  <GlobalOutlined style={{ fontSize: '23px', color: '#1f91ff', cursor: 'pointer' }} />
-                </div>
-              </Dropdown>
-            </Space>
+            <div style={{ display: "flex" }}>
+              <div>
+                {" "}
+                <Dropdown overlay={userMenu}>
+                  <div style={{ cursor: "pointer" }}>
+                    <Space>
+                      <Avatar size={32} icon={<UserOutlined />} />
+                      <span>Avtandil</span>
+                    </Space>
+                  </div>
+                </Dropdown>
+              </div>
+              <div style={{marginTop: 5, marginLeft: 15}}>
+                {" "}
+                <Dropdown overlay={languageMenu}>
+                  <div onClick={(e) => e.preventDefault()}>
+                    <GlobalOutlined
+                      style={{
+                        fontSize: "26px",
+                        color: "#1f91ff",
+                        cursor: "pointer",
+                      }}
+                    />
+                  </div>
+                </Dropdown>
+              </div>
+            </div>
           </div>
 
           {/* </div> */}
-
-
         </Header>
         <Content
           className="site-layout-background"
           style={{
-            margin: '35px 16px',
+            margin: "35px 16px",
             // padding: 18,
             // minHeight: 280,
             // textAlign: "center"
           }}
         >
-
-
-
-
-
           <Switch>
             <Route exact path="/">
               <Redirect to={`${HOME_PAGE}/dashboard`} />
@@ -328,8 +374,6 @@ function Home() {
               <Dashboard />
             </Route>
           </Switch>
-
-
         </Content>
       </LayoutAnt>
     </LayoutAnt>
